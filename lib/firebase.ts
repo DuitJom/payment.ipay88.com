01d9510c2c2a6263ae0a4ffe26bf6c45fc02b198
Duitjom Payment Hub <288976667+DuitJom@users.noreply.github.com>
2026-09-14 04:42:18 +0800

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,15 +2,18 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env."AIzaSyCKb-QOYSTP0scv0UXmraluMe3xFtfIH_0",
-  authDomain: process.env."duitjom-sign-up-in.firebaseapp.com",
-  projectId: process.env."duitjom-sign-up-in",
-  storageBucket: process.env."duitjom-sign-up-in.firebasestorage.app",
-  messagingSenderId: process.env."246834525616",
-  appId: process.env."1:246834525616:web:a8429bda0166e6e03c1275",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length > 0
+  ? getApp()
+  : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
