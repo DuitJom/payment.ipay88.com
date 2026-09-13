0c5002d7016009d7314bc31cc5ca197fbcf67463
Duitjom Payment Hub <288976667+DuitJom@users.noreply.github.com>
2026-09-14 04:29:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Firebase Auth",
+  description: "Firebase Authentication dengan Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
   return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-  )
+    <html lang="ms">
+      <body>{children}</body>
+    </html>
+  );
 }
