17f9d17530eae5684e6a91299c448dbe7b662a62
Duitjom Payment Hub <288976667+DuitJom@users.noreply.github.com>
2026-09-14 02:41:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
-import { ClerkProvider } from '@clerk/nextjs'
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body>{children}</body>
       </html>
-    </ClerkProvider>
   )
 }
